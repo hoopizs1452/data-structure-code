80497be9fb99aa4f46bcc03f5ea0aea695d20644
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-11-09 14:24:10 +0800

    Update and rename 3-1 使用堆疊進行.cpp to 3-1 使用堆疊進行Push和Pop.cpp

--- 3-1 使用堆疊進行Push和Pop.cpp
+++ 3-1 使用堆疊進行Push和Pop.cpp
@@ -5,7 +5,6 @@ private:
     int top;
     int capacity;
     int *array;
-    void DoubleCapacity();
 public:
     Stack():top(-1),capacity(1){
         array = new int[capacity];
@@ -14,46 +13,19 @@ public:
     void Pop();
     void PrintStack();
 };
-void Stack::DoubleCapacity(){
-
-    capacity *= 2;
-    int *newStack = new int[capacity]; 
-
-    for (int i = 0 ; i < capacity/2; i++) {
-        newStack[i] = array[i];
-    }
-
-    delete [] array;
-    array = newStack;
-}
 void Stack::Push(int x){
-
-    if (top == capacity - 1) {
-        DoubleCapacity();
-    }
     array[++top] = x;
 }
 
 void Stack::Pop(){
-
-    if (top == -1) {
-        std::cout << "Stack is empty.\n";
-        return;
-    }
     top--;
 }
 void Stack::PrintStack()
 {
-    if (top == -1) {
-        std::cout << "Stack is empty.\n";
-    }
-    else
-    {
-        for (int i = top;i>=0;i--)
+    for (int i = top;i>=0;i--)
         {
             std::cout <<array[i]<<" ";
         }
-    }
 }
 int main(int argc, char *argv[])
 {
